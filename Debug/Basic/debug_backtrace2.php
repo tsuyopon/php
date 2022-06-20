0fec08d5f2dc5008f5c7730810c29b90f2fc498c
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2022-06-21 03:33:13 +0900

    mod

--- Debug/Basic/debug_backtrace2.php
+++ Debug/Basic/debug_backtrace2.php
@@ -2,6 +2,8 @@
 /*
  * クラス内でdebug_backtrace()を利用してファイル名・行数・関数名・クラス名・型名を取得する。また引き渡された値を表示する
  * cf. http://raining.bear-life.com/php/php%E3%81%A7%E9%96%8B%E7%99%BA%E6%99%82%E3%81%AB%E4%BD%BF%E7%94%A8%E3%81%97%E3%81%A6%E3%81%84%E3%82%8B%E3%83%87%E3%83%90%E3%83%83%E3%82%AFdebug%E6%96%B9%E6%B3%95
+ *
+ * 公式リファレンス: https://www.php.net/manual/ja/function.debug-backtrace.php
  */
 
 $arr = array('aaa', 'bbb', 'ccc');
